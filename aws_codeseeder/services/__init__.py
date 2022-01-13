4c8d6ecf5ec6086a5b97e39aefebaa39f97b3cb1
Chauncy McCaughey <chamcca@amazon.com>
2022-01-13 14:44:46 -0600

    eliminate circular import

--- aws_codeseeder/services/__init__.py
+++ aws_codeseeder/services/__init__.py
@@ -20,7 +20,6 @@ import boto3
 import botocore
 
 from aws_codeseeder import LOGGER, __version__
-from aws_codeseeder.services import cfn, cloudwatch, codebuild, iam, s3  # noqa: F401
 
 
 def get_botocore_config() -> botocore.config.Config:
