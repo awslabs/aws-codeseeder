a27ebe85d2889cd9367ddb353bbebb447fe353f7
malachi-constant <lucascchanson@gmail.com>
2024-08-19 08:52:38 -0700

    chore: change logging for npm_mirror_support script

--- aws_codeseeder/resources/npm_mirror_support.py
+++ aws_codeseeder/resources/npm_mirror_support.py
@@ -40,9 +40,9 @@ def main(url: str) -> None:
         if key in creds.keys():
             ssl_token = creds[key]["ssl_token"] if creds[key].get("ssl_token") else None
             subprocess.call(["npm", "config", "set", f"{url.replace('https:', '')}:_auth={ssl_token}"])
-        logger.info("Calling npm config with %s", url)
     else:
-        logger.info("'AWS_CODESEEDER_NPM_MIRROR_SECRET' is not set")
+        print("'AWS_CODESEEDER_NPM_MIRROR_SECRET' is not set")
+    print(f"Calling npm config with {url}")
     subprocess.call(["npm", "config", "set", "registry", url])
 
 
